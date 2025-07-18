33141539aaf4f845e22708b17c41e9678044473e
BoscoFZ <70411098+BoscoFZ@users.noreply.github.com>
2025-07-18 19:51:20 +0700

    Update download.py

--- download.py
+++ download.py
@@ -23,12 +23,6 @@ def get_chrome_options(download_dir):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-plugins")
-    chrome_options.add_argument("--disable-images")
-    chrome_options.add_argument("--disable-web-security")
-    chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
 
     prefs = {
         "download.default_directory": os.path.abspath(download_dir),
